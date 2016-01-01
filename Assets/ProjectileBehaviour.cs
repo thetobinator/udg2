9c75db9a19791095e49fbadfb450a5ac69019bb8
Tobias Opfermann <tobias@igame3d.com>
2016-01-01 17:51:26 +0100

    added projectile behaviour

---
+++ Assets/ProjectileBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileBehaviour : MonoBehaviour {
+
+	public float maxLifetime = 2.0f;
+	public float speed = 20.0f;
+
+	float m_lifeTime;
+
+	// Use this for initialization
+	void Start () {
+		m_lifeTime = maxLifetime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + transform.up * speed * Time.deltaTime;
+		m_lifeTime -= Time.deltaTime;
+		if (m_lifeTime < 0.0f) {
+			Destroy( gameObject );
+		}
+	}
+
+
+}
