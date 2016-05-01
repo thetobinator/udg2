1d593f76222eb6f3df6c9b4a9c0a9bac49c28c44
Tobias Opfermann <tobias@igame3d.com>
2016-05-01 12:53:33 +0200

    added dummy destination markers

---
+++ Assets/Scripts/DestinationMarkerBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestinationMarkerBehaviour : MonoBehaviour {
+	private float m_lifeTime = 1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_lifeTime -= Time.deltaTime;
+		transform.localScale = new Vector3( m_lifeTime, m_lifeTime, m_lifeTime );
+		if (m_lifeTime <= 0.0f) {
+			Destroy ( gameObject );
+		}
+	}
+}
