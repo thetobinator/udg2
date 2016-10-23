e8ff97c7902f6a6736b9624c1798dd00b3cc1e4d
igame3dbill <bill@igame3d.com>
2016-10-22 18:18:32 -0600

    Head Component contains Breakable barricades code, meant to explodeImmediately with the current_health but something zombies hitting the barricades is not working   yet.

--- Assets/Scripts/HealthComponent.cs
+++ Assets/Scripts/HealthComponent.cs
@@ -2,21 +2,42 @@
 using System.Collections;
 
 public class HealthComponent : MonoBehaviour {
-	public float initialHealth = 100.0f;
-    
+	public float initialHealth = 100.0f;   
 	public float current_health;
 
-	// Use this for initialization
-	void Start () {
-		reanimate ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //breakable barricades code.
+    private GameObject thisObject;
+    public Rigidbody rb;
 
-	public void dealDamage( float damage ) {
+    public float radius = 15.0F;
+    public float power = 110.0F;
+	private Rigidbody hasRigidBody;
+	private Vector3 childscale;
+	
+        void Awake()
+        {
+            rb = this.GetComponent<Rigidbody>();
+        }
+
+
+		void EnableRagdoll() {
+			rb.isKinematic = false;
+			rb.detectCollisions = true;
+		}
+		void DisableRagdoll() {
+			rb.isKinematic = true;
+			rb.detectCollisions = false;
+		}
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        if (rb != null) { EnableRagdoll(); }
+        reanimate();
+    }
+
+    public void dealDamage( float damage ) {
         current_health -= damage;
 	}
 
@@ -27,4 +48,39 @@ public class HealthComponent : MonoBehaviour {
 	public void reanimate() {
         current_health = initialHealth;
 	}
+
+    public void explodeInstantly()
+    {
+        current_health = 0;
+       this.tag = "Untagged";
+      
+            if (this.GetComponent<Rigidbody>() != null)
+            {
+                DisableRagdoll();
+            }
+            // iterate over child objects and give them rigidbody
+            foreach (Transform child in transform)
+            {
+                child.gameObject.tag = "Wood";
+                hasRigidBody = child.gameObject.GetComponent<Rigidbody>();
+                // break the object apart
+                if (hasRigidBody == null)
+                {
+                    Vector3 childscale = child.gameObject.transform.localScale;
+                    childscale.x = childscale.x * 0.45F;
+                    childscale.y = childscale.y * 0.45F;
+                    childscale.z = childscale.z * 0.75F;
+                    child.gameObject.transform.localScale = new Vector3(childscale.x, childscale.y, childscale.z);
+                    Rigidbody gameObjectsRigidBody = child.gameObject.AddComponent<Rigidbody>(); // Add the rigidbody.
+                    gameObjectsRigidBody.mass = 1; // Set the GO's mass to 5 via the Rigidbody.
+                                                   // Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+                    if (gameObjectsRigidBody != null)
+                    {
+                        gameObjectsRigidBody.AddExplosionForce(power, child.gameObject.transform.position, radius, 3.0F);
+                    }
+                }
+            }
+        }
+   
 }
