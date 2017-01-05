31f241dc47596e5c9b1fea0dea0372bcc90a7d6d
Tobias Opfermann <tobias@igame3d.com>
2017-01-05 13:04:14 +0100

    added crosshair behaviour

---
+++ Assets/Scripts/CrosshairBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrosshairBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DebugTint d = GetComponent<DebugTint> ();
+		if (d != null) {
+			d.tintColor = Color.green;
+			Ray ray = Camera.main.GetComponent<Camera> ().ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit = new RaycastHit ();
+			if (Physics.Raycast (ray, out hit)) {
+				GameObject obj = ZombieBehavior.getRootObject( hit.collider.gameObject.transform.parent.gameObject );
+				if (obj.tag == "Human") {
+					d.tintColor = Color.red;
+				}
+			}
+		}
+	}
+}
